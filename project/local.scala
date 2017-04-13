195db58f4191a82ae959512c075753b14c897e80
Eugene Cheverda <eugene.cheverda@gmail.com>
2017-04-12 21:33:13 -0500

    Cleaned deps and updated pulse plugin version

--- project/local.scala
+++ project/local.scala
@@ -5,21 +5,7 @@ import sbt.Keys._
 
 object local {
   object dependencies {
-
     object versions {
-      val log4s = "1.3.4"
-      object twitter {
-        val finagle = "6.42.0"
-      }
-    }
-
-    object finagle {
-      val core   = "com.twitter" %% "finagle-http"   % versions.twitter.finagle
-    }
-
-
-    object log4s {
-      val core = "org.log4s" %% "log4s" % versions.log4s
     }
   }
 
--- project/pulse.sbt
+++ project/pulse.sbt
@@ -1,3 +1,3 @@
 resolvers += Resolver.url("impulse bintray", url("https://dl.bintray.com/impulse-io/sbt-plugins"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("impulse-io" % "sbt-pulse" % "1.0.7")
+addSbtPlugin("impulse-io" % "sbt-pulse" % "1.0.9")
