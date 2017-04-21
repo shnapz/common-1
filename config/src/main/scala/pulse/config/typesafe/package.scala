3c43c03187a80ca62252e01276bc25168e3f7403
akabas <andrii.kabas@zorallabs.com>
2017-04-21 22:41:31 +0300

    Added support of classpath source for mutable config scenario

--- config/src/main/scala/pulse/config/typesafe/package.scala
+++ config/src/main/scala/pulse/config/typesafe/package.scala
@@ -1,27 +1,32 @@
 package pulse.config
 
+import java.io.File
+
+import cats.implicits._
 import com.typesafe.config.ConfigFactory
 import fs2.Task
 import fs2.util.Attempt
-import pulse.common.exceptions.NotSupportedException
+import pulse.common.exceptions.{ConfigException, NotSupportedException}
 
 import scala.util.{Failure, Success, Try}
 
 package object typesafe {
+
   import Task._
+
+  val defaultConfigName = "application.conf"
 
   implicit val immutableBuilder: ImmutableBuilder = new ImmutableBuilder {
     def apply(source: Source): Task[Conf] = for {
       c <- delay {
         source match {
-          case Source.Classpath     => ConfigFactory.defaultApplication()
+          case Source.Classpath => ConfigFactory.defaultApplication()
           case Source.FileSource(f) => ConfigFactory.parseFile(f)
-          case Source.Raw(config)   => ConfigFactory.parseString(config)
+          case Source.Raw(config) => ConfigFactory.parseString(config)
         }
       }
     } yield new TypesafeConf(c)
   }
-
 
   implicit val mutableBuilder: MutableBuilder = new MutableBuilder {
     def apply(source: Source): fs2.Stream[Task, Attempt[Conf]] = source match {
@@ -31,6 +36,13 @@ package object typesafe {
           case Failure(err) => Left(err)
         }
       }
+
+      case Source.Classpath =>
+        ClassLoader.getSystemClassLoader.getResource(defaultConfigName).some match {
+          case Some(url) => apply(Source.FileSource(new File(url.getFile)))
+          case None => fs2.Stream.eval(fail(ConfigException(s"Default config $defaultConfigName has not found on the classpath")))
+        }
+
       case x => fs2.Stream.eval(fail(NotSupportedException(s"Source '$x' is not supported")))
     }
   }
